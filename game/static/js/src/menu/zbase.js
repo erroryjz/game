53c78cc21369fd81b398dc1afc9705c2a9e6cb5f
handwind <yejunzhe144@qq.com>
2022-09-08 19:06:33 +0800

    make menu but cannot jump to playground

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -4,14 +4,16 @@ class AcGameMenu{
         this.$menu = $(`
 <div class="ac-game-menu">
     <div class="ac-game-menu-field">
-        <div class="ac-game-field-item ac-game-field-item-single">
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
             单人模式
 
         </div>
-        <div class="ac-game-field-item ac-game-field-item-multi">
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-multi-mode">
             多人模式
         </div>
-        <div class="ac-game-field-item ac-game-field-item-settings">
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
             设置
         </div>
 
@@ -20,9 +22,33 @@ class AcGameMenu{
 </div>
 `);
         this.root.$ac_game.append(this.$menu);
-        this.single = this.$menu.find('.ac-game-field-item-single');
-        this.multi = this.$menu.find('.ac-game-field-item-multi');
-        this.settings = this.$menu.find('.ac-game-field-item-settings');
+        this.single_mode = this.$menu.find('.ac-game-menu-field-item-single-mode');
+        this.multi_mode = this.$menu.find('.ac-game-menu-field-item-multi-mode');
+        this.settings = this.$menu.find('.ac-game-menu-field-item-settings');
+        this.start();
+    }
 
+    start(){
+    this.add_listening_events();
+    }
+    add_listening_events(){
+        let outer = this;
+        this.$single_mode.click(function(){
+            outer.hide();
+            outer.root.playground.show();
+        });
+        this.$multi_mode.click(function(){
+            console.log("click2");
+        });
+        this.$settings.click(function(){
+            console.log("click3");
+        });
+    }
+    show(){ //显示menu界面
+        this.$menu.show();
+    }
+    hide(){ //关闭menu界面
+        this.$menu.hide();
     }
 }
+
