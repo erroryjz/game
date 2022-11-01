03d010b7eb716d0df89fdb4718d5343d46c65a5d
y jz <yejunzhe144@qq.com>
2022-11-01 09:51:13 +0800

    add ranklist, almost complete

--- game/urls/settings/index.py
+++ game/urls/settings/index.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from game.views.settings.getinfo import InfoView
 from game.views.settings.register import register
+from game.views.settings.ranklist import RanklistView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -12,5 +13,6 @@ urlpatterns = [
     path("token/refresh/", TokenRefreshView.as_view(), name="settings_token_refresh"),
     path("getinfo/", InfoView.as_view(), name="settings_getinfo"),
     path("register/", register, name="settings_register"),
+    path("ranklist/", RanklistView.as_view(), name="settings_ranklist"),
     path("acwing/", include("game.urls.settings.acwing.index")),
 ]
